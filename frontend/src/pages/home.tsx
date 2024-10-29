7d4df0bbdf040294b9d5f4cd016ddb6aa30f62c2
gllm-dev <guillem@openfort.xyz>
2024-10-29 16:34:52 +0100

    feat: working sample

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -40,6 +40,7 @@ export const HomePage: FC = () => {
 
     function mintNFT() {
         console.log("Minting NFT...");
+        document.getElementById("mint")!.innerHTML = `<div class="spinner"></div>`;
         if (!initDataRaw) {
             console.error("No init data found");
             return;
@@ -49,9 +50,20 @@ export const HomePage: FC = () => {
             console.log("NFT minted!");
             console.log("Transaction hash:", res);
             document.getElementById("msg")!.innerHTML = `<a href="https://www.oklink.com/amoy/tx/${res}" target="_blank">View transaction</a>`;
+            document.getElementById("mint")!.innerHTML = `<button className="mint-button" onClick={mintNFT}>Mint</button>`;
         }).catch((error) => {
             console.error("Failed to mint NFT:", error);
             document.getElementById("msg")!.innerHTML = "Failed to mint NFT";
+            document.getElementById("mint")!.innerHTML = `<button className="mint-button" onClick={mintNFT}>Mint</button>`;
+        });
+    }
+
+    function logout() {
+        openfortService.logout().then(() => {
+            console.log("Logged out");
+            window.location.reload();
+        }).catch((error) => {
+            console.error("Failed to logout:", error);
         });
     }
 
@@ -69,11 +81,11 @@ export const HomePage: FC = () => {
                   <button className="connect-button" onClick={() => openfortService.setAutomaticRecoveryMethod(initDataRaw!)}>Connect Wallet</button>
                 ) : embeddedState === EmbeddedState.READY ? (
                     <>
-                        <div>
+                        <div id="mint">
                             <button className="mint-button" onClick={mintNFT}>Mint</button>
                         </div>
                         <div id="msg"></div>
-                        <a onClick={() => openfortService.logout()} className="disconnect-text">Logout</a>
+                        <a onClick={logout} className="disconnect-text">Logout</a>
                     </>
                 ) : (
                   <div className="spinner"></div>
